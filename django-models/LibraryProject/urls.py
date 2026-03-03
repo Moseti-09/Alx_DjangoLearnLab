cabaa4de66013532688cb2f2077ffdd4ee215bae
Cliff Moseti <cmbichanga@gmail.com>
2026-03-03 05:57:45 +0300

    Add function-based and class-based views + URL routing for relationship_app

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,23 +1,26 @@
-"""
-URL configuration for LibraryProject project.
+from django.urls import path
+from .views import list_books , LibraryDetailView
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),   # ← This line connects the app
+    path('books/', list_books, name="list_books"),
+    path('library/<int:pk>/', LibraryDetailView.as_view, name="library_detail"),
+    path('login/', LoginView.as_view(template_name="login")),
+    path('register/', views.register_view(template_name="register")),
+    path('logout/', LogoutView.as_view(template_name="logout")),
+    path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/edit/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", views.delete_book, name="delete_book"),
+]
+
+
+urlpatterns = [
+    # Book permission-based views
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:book_id>/", views.delete_book, name="delete_book"),
 ]
