49a97ba38006bf1d817005e9041dc03e9d3a287b
Cliff Moseti <cmbichanga@gmail.com>
2026-03-04 03:17:16 +0300

    Add Posts and comments with viewsets, permissions, pagination and search

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,16 +1,25 @@
-from django.urls import path
-from .views import RegisterView, LoginView, UserDetailView, follow_user, unfollow_user
-from rest_framework.routers import DefaultRouter
+"""
+URL configuration for social_media_api project.
 
-
-router = DefaultRouter()
-router.register('users', UserDetailView)
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', UserDetailView.as_view(), name='user-detail'),
-    path('follow/<int:user_id>/', follow_user, name='follow_user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow_user'),
+    path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
+    path('api/', include('notifications.urls')),
 ]
